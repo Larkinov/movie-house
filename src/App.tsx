43db4219d1f6efb3aed6c4d7b1e21ca9627b4ad4
Larkinov <buyantolarkinov@gmail.com>
2023-08-09 01:03:31 +0700

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { kp } from "./test";
 
-function App() {
+const App:React.FC = function() {
+
+  const   getData = async() => {
+    const {data} = await kp.movie.getById(666);
+    console.log(data);
+  }
+
+  getData();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>ss</div>
+    </>
   );
 }
 
---
+++ src/test.tsx
@@ -0,0 +1,3 @@
+import { KinopoiskDev } from '@openmoviedb/kinopoiskdev_client';
+
+export const kp = new KinopoiskDev('MAF95XR-1234TVF-PQW68A1-RKA4E5T');
