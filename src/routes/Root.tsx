8e903f37a277e9f5c41bac8dc12220ee09a9e26d
Larkinov <buyantolarkinov@gmail.com>
2023-08-22 00:14:19 +0700

    testing threee

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import style from "../scss/Root.module.scss";
-<<<<<<< HEAD
-import Header from "../components/Header/Header";
 
 const Root: React.FC = function () {
   // fetch("https://kinopoiskapiunofficial.tech/api/v2.2/films", {
@@ -15,45 +13,29 @@ const Root: React.FC = function () {
   //   .then((res) => res.json())
   //   .then((json) => console.log(json))
   //   .catch((err) => console.log(err));
+
   return (
     <>
       <div className={style.main}>
         <div className={style.container}>
-          <Header />
+          <header>Header</header>
+          <ul>
+            <li>
+              <Link to={"/home"}>home</Link>
+            </li>
+            <li>
+              <Link to={"/serials"}>serials</Link>
+            </li>
+            <li>
+              <Link to={"/films"}>films</Link>
+            </li>
+            <li>
+              <Link to={"/cartoons"}>cartoons</Link>
+            </li>
+          </ul>
           <Outlet />
           <footer>footer</footer>
         </div>
-=======
-
-const Root: React.FC = function () {
-  fetch("https://kinopoiskapiunofficial.tech/api/v2.2/films", {
-    method: "GET",
-    headers: {
-      "X-API-KEY": "6219ce44-66a2-458a-9cf4-bcc570cecc00",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.log(err));
-  return (
-    <>
-      <div className={style.container}>
-        <header>Header</header>
-        <ul>
-          <li>
-            <Link to={"/serials"}>serials</Link>
-          </li>
-          <li>
-            <Link to={"/films"}>films</Link>
-          </li>
-          <li>
-            <Link to={"/cartoons"}>cartoons</Link>
-          </li>
-        </ul>
-        <Outlet />
-        <footer>footer</footer>
->>>>>>> b94b4ac93a638a9cea30bdf97c00d1d4d675210d
       </div>
     </>
   );
